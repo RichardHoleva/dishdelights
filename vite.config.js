7fcc743f482a79fb5eb435cd78950b69a10e2c0a
RichardHoleva <eaa24rhol@students.eaaa.dk>
2025-02-14 04:40:55 +0100

    pleasegodplease

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   plugins: [react()],
-  base: isProd ? "/dishdelights/" : "/",
-  build: {
-    outDir: 'docs',
-  },
-
+  base: "/dishdelights/",
 });
-
