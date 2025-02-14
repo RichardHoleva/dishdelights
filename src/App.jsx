5589bde1c55bf1ba5c360671277af7cb5952c7a7
RichardHoleva <eaa24rhol@students.eaaa.dk>
2025-02-14 03:52:20 +0100

    PleaseWork

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import DishDelights from './pages/DishDelights';
 import PersonalFavorites from './pages/PersonalFavorite';
 import About from './pages/About';
-import Contact from './pages/Contact'; // Import the Contact component
+import Contact from './pages/Contact';
 
 export default function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -25,11 +25,11 @@ export default function App() {
         </div>
         <nav className={isMenuOpen ? 'nav-open' : ''}>
           <ul>
-            <li><a href="/" className="nav-link">Home</a></li>
-            <li><a href="/dishdelights" className="nav-link">Recipes</a></li>
-            <li><a href="/personalfavorites" className="nav-link">Personal Favorites</a></li>
-            <li><a href="/about" className="nav-link">About</a></li>
-            <li><a href="/contact" className="nav-link">Contacts</a></li>
+            <li><Link to="/" className="nav-link">Home</Link></li>
+            <li><Link to="/dishdelights" className="nav-link">Recipes</Link></li>
+            <li><Link to="/personalfavorites" className="nav-link">Personal Favorites</Link></li>
+            <li><Link to="/about" className="nav-link">About</Link></li>
+            <li><Link to="/contact" className="nav-link">Contacts</Link></li>
           </ul>
         </nav>
       </header>
@@ -38,7 +38,7 @@ export default function App() {
         <Route path="/dishdelights" element={<DishDelights />} />
         <Route path="/personalfavorites" element={<PersonalFavorites />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} /> {/* Add the Contact route */}
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <footer className="main-footer">
         <p>
